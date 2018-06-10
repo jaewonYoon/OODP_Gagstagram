473ba35b5d35c812b6d2e004cfbca0ed6562bed0
dlgur1994 <38805855+dlgur1994@users.noreply.github.com>
2018-06-10 14:53:10 +0900

    Create Fall.java

---
+++ final_iteration/Hyuk/Fall.java
@@ -0,0 +1,11 @@
+import javax.swing.JOptionPane;
+
+public class Fall extends Season{
+	
+	void tellspring() {};
+    void tellsummer() {};
+    void tellfall() {
+    	JOptionPane.showMessageDialog(null, "가을입니다");
+    };
+    void tellwinter() {};
+}
