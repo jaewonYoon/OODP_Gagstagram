a2e07b55c375bcd7f4ede7b0a2882d6b69f4c6dc
dlgur1994 <38805855+dlgur1994@users.noreply.github.com>
2018-06-10 15:14:04 +0900

    Update Fall.java

--- final_iteration/Hyuk/Fall.java
+++ final_iteration/Hyuk/Fall.java
@@ -2,7 +2,7 @@ import javax.swing.JOptionPane;
 
 public class Fall extends Season{
 	
-	void tellspring() {};
+    void tellspring() {};
     void tellsummer() {};
     void tellfall() {
     	JOptionPane.showMessageDialog(null, "It's Fall");
