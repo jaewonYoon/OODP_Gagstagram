0abe108e77bc549fe86daa6547ce834049a29a13
dlgur1994 <38805855+dlgur1994@users.noreply.github.com>
2018-06-10 14:54:23 +0900

    Create Summer.java

---
+++ final_iteration/Hyuk/Summer.java
@@ -0,0 +1,11 @@
+import javax.swing.JOptionPane;
+
+public class Summer extends Season{
+	
+	void tellspring() {};
+    void tellsummer() {
+    	JOptionPane.showMessageDialog(null, "여름입니다");
+    };
+    void tellfall() {};
+    void tellwinter() {};
+}
