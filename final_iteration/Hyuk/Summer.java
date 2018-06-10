612ebebb8b68a5b0e0d481ccdda7145aec064737
dlgur1994 <38805855+dlgur1994@users.noreply.github.com>
2018-06-10 15:44:32 +0900

    Update Summer.java

--- final_iteration/Hyuk/Summer.java
+++ final_iteration/Hyuk/Summer.java
@@ -1,11 +1,7 @@
 import javax.swing.JOptionPane;
 
 public class Summer extends Season{
-	
-    void tellspring() {};
-    void tellsummer() {
+    void tellseason() {
     	JOptionPane.showMessageDialog(null, "It's Summer");
     };
-    void tellfall() {};
-    void tellwinter() {};
 }
