369c51929dabea7bda70f71e8dcc513369ca59e1
dlgur1994 <38805855+dlgur1994@users.noreply.github.com>
2018-06-10 15:10:51 +0900

    Update Fall.java

--- final_iteration/Hyuk/Fall.java
+++ final_iteration/Hyuk/Fall.java
@@ -5,7 +5,7 @@ public class Fall extends Season{
 	void tellspring() {};
     void tellsummer() {};
     void tellfall() {
-    	JOptionPane.showMessageDialog(null, "가을입니다");
+    	JOptionPane.showMessageDialog(null, "It's Fall");
     };
     void tellwinter() {};
 }
